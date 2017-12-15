3219eaba63d7ad3697fcb39fc0fd20bba1c01f8c
ServiceFamily <brservice@comcast.net>
2017-12-14 22:27:53 -0500

    i hate java, i don't understand it

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,6 +6,24 @@ public class Main {
 	// write your code here
         Scanner kb = new Scanner(System.in);
         String input = kb.nextLine();
-        
+        /*
+        Going to do comments because I can't do this and my partner's code makes no sense.
+
+        I would take the input the user has given and take the two things to compare. Then
+        I would see if how they were being compared (&& or ||) and then print out T's amd F's
+        for when they are true or false and then print to the right of them what the final result
+        would be.
+
+        Example:
+        Input: X || Y
+
+        X    Y    Result
+        T    T    T
+        T    F    T
+        F    T    T
+        F    F    F
+
+        Hope this is good enough because I can't put this in code
+         */
     }
 }
